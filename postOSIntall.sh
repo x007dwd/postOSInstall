60321249d4ce42103ff8f03c52242fa2c807b34a
bobin <x007dwd@gmail.com>
2016-12-01 17:24:01 +0800

    Update postOSIntall.sh

--- postOSIntall.sh
+++ postOSIntall.sh
@@ -1,8 +1,39 @@
+sudo ./flash.sh -S 14580MiB jetson-tk1 mmcblk0p1
+sudo apt-mark hold xserver-xorg-core
+
+## config bash-completion
+sudo apt-get install bash-completion command-not-found
+sudo apt-file update &>/dev/null
+sudo update-command-not-found &>/dev/null
+exit
+
+## install some drivers
+cd /tmp/
+mkdir GrinchKernel
+cd GrinchKernel
+## Download Grinch Kernel
+
+wget http://www.jarzebski.pl/files/jetsontk1/grinch-21.3.4/zImage
+wget http://www.jarzebski.pl/files/jetsontk1/grinch-21.3.4/jetson-tk1-grinch-21.3.4-modules.tar.bz2
+wget http://www.jarzebski.pl/files/jetsontk1/grinch-21.3.4/jetson-tk1-grinch-21.3.4-firmware.tar.bz2
+### Update kernel
+
+sudo tar -C /lib/modules -vxjf jetson-tk1-grinch-21.3.4-modules.tar.bz2
+sudo tar -C /lib -vxjf jetson-tk1-grinch-21.3.4-firmware.tar.bz2
+sudo cp zImage /boot/zImage
+
+
+## config timezone and update time
+sudo dpkg-reconfigure tzdata
+sudo service ntp stop
+sudo ntpdate pool.ntp.org
+sudo service ntp start
+
 ## config vim
-sh <(curl https://j.mp/spf13-vim3 -L)
+## sh <(curl https://j.mp/spf13-vim3 -L)
 
 ## config ssh
-sudo apt-get install openssh-server
+## sudo apt-get install openssh-server
 
 ## install chrome
 cd /tmp/
@@ -20,18 +51,14 @@ sudo cat ipv6-hosts/hosts >> /etc/hosts
 sudo apt-get install nautilus-open-terminal
 nautilus -q
 
-## install dropbox
-cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
-~/.dropbox-dist/dropboxd
-## install shadowsocks
-sudo add-apt-repository ppa:hzwhuang/ss-qt5
-sudo apt-get update
-sudo apt-get install shadowsocks-qt5
-
 ## install build system for c/cpp
 sudo apt-get install build-essential cmake cmake-curses-gui
 
+
 ## install some cpp package
+sudo apt-get install libopencv libopencv-dev
+sudo apt-get install libboost-all
+
 sudo apt-get install libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev protobuf-compiler
 sudo apt-get install --no-install-recommends libboost-all-dev   libatlas-base-dev
 sudo apt-get install libgflags-dev libgoogle-glog-dev liblmdb-dev
@@ -57,8 +84,17 @@ git clone https://github.com/dji-sdk/Onboard-SDK-ROS.git
 cd ..
 catkin_make
 
-## config cuda
-sudo dpkg -i cuda-repo-ubuntu1404-8-0-local_8.0.44-1_amd64.deb
-sudo apt-get update
-sudo apt-get install cuda
+## config ros compatible with opencvtegta
+sudo cp /opt/ros/indigo/lib/pkgconfig/cv_bridge.pc /opt/ros/indigo/lib/pkgconfig/cv_bridge.pc-bak
+sudo cp /opt/ros/indigo/lib/pkgconfig/image_geometry.pc  /opt/ros/indigo/lib/pkgconfig/image_geometry.pc-bak
+sudo cp /opt/ros/indigo/share/cv_bridge/cmake/cv_bridgeConfig.cmake  /opt/ros/indigo/share/cv_bridge/cmake/cv_bridgeConfig.cmake-bak
+sudo cp /opt/ros/indigo/share/image_geometry/cmake/image_geometryConfig.cmake /opt/ros/indigo/share/image_geometry/cmake/ image_geometryConfig.cmake-bak
+
+## these files are in foleder of ros-opencvtegra
+sudo cp ros-opencvtegra/pkgconfig/cv_bridge.pc /opt/ros/indigo/lib/pkgconfig/cv_bridge.pc
+sudo cp ros-opencvtegra/image_geometry.pc /opt/ros/indigo/lib/pkgconfig/image_geometry.pc
+sudo cp ros-opencvtegra/cv_bridgeConfig.cmake /opt/ros/indigo/share/cv_bridge/cmake/cv_bridgeConfig.cmake
+sudo cp ros-opencvtegra/image_geometryConfig.cmake /opt/ros/indigo/share/image_geometry/cmake/image_geometryConfig.cmake
+
+
 ## config Eigen and sophus, g2o
